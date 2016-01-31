96f36376708ae4e456369685bd7b690d7f67e00c
Bideep <bbideep@gmail.com>
2016-01-30 23:36:07 -0600

    Create downloadjdk.sh

---
+++ downloadjdk.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if $# -eq 0;then
+  echo "USAGE ./downloadjdk.sh <URL of the JDK like http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz>"
+  exit 1
+fi
+
+#JDKURL like http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+JDKURL=$1
+FILENAME=`echo $JDKURL |awk -F"/" '{print $NF}'`
+
+curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" $JDKURL > $FILENAME
