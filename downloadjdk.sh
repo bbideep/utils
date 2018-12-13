36065aa2635ada00a6e2ce3a947a365915608cd8
Bideep <bbideep@gmail.com>
2018-12-13 16:05:08 -0600

    Update downloadjdk.sh

--- downloadjdk.sh
+++ downloadjdk.sh
@@ -6,6 +6,7 @@ if [ $# -lt 1 ];then
 fi
 
 #JDKURL like http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+#https://download.oracle.com/otn-pub/java/jdk/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_linux-x64_bin.tar.gz
 JDKURL=$1
 FILENAME=`echo $JDKURL |awk -F"/" '{print $NF}'`
 
