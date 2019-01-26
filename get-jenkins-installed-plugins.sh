ae14d4866cb5b70aadc2cdfdac6f316a47dc2e1c
Bideep <bbideep@gmail.com>
2019-01-26 11:43:40 -0600

    Create get-jenkins-installed-plugins.sh

---
+++ get-jenkins-installed-plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+JENKINS_HOST=<jenkins_admin_username>:<password>@<JENKINS_IP>:<JENKINSPORT>
+
+curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/'
